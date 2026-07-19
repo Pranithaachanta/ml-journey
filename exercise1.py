b08a023b55cbbf803d4add3e645aa0bf7ea76687
Pranitha Achanta <pranithaachanta@gmail.com>
2026-07-19 01:23:22 -0600

    exercise 1 complete

---
+++ exercise1.py
@@ -0,0 +1,9 @@
+character_name = "Shivansh"
+story_name = "All I ever need was you"
+chapters_read = 19
+rating = 8.5
+would_recommend = True
+author = "someone"
+genre = "romance"
+
+print(f"I read {chapters_read} chapters of {story_name} and rated it {rating}/10. Recommend? {would_recommend}. The author is {author} and the genre of this book is {genre} ")
