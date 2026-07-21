3a09ce8cc9207c6ecf3605bec9bb88b9ff433391
Pranitha Achanta <pranithaachanta@gmail.com>
2026-07-21 01:18:38 -0600

    learned split

--- day3_exercise.py
+++ day3_exercise.py
@@ -3,5 +3,8 @@ author = "someone"
 
 print(f"{story_name.upper()}")
 print(len(story_name))
-print(f"First word: {story_name[0:3]}")
+print(f"First word: {story_name[:3]}")
 print(f"Reverse: {author[::-1]}")
+
+words =story_name.split()
+print(f"First word: {words[5]}")
