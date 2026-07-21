2ce6ddbbb2d81ab6833fbafc0f8e3208ea698310
Pranitha Achanta <pranithaachanta@gmail.com>
2026-07-21 01:01:28 -0600

    day 3 - strings and operators

---
+++ day-3.py
@@ -0,0 +1,27 @@
+name = 'pranitha is'
+print(name[9:])
+
+
+# First exercise
+name = "Pranitha"
+goal = "ML Engineer"
+
+#string methods
+print(f"Uppercase: {name.upper()}")
+print(f"Lowercase: {goal.lower()}")
+print(f"name Length:{len(name)} letters")
+
+#slicing
+print(f"First 3 lettes: {name[0:3]}")
+print(f"Last 3 letters: {name[-3:]}")
+print(f"Name Backwards: {name[::-1]}")
+
+#Operators
+x=25
+y=4
+print(f"Sum: {x+y}")
+print(f"Product: {x*y}")
+print(f"Divison: {x/y}")
+print(f"Floor Divison: {x//y}")
+print(f"Remainder: {x%y}")
+print(f"Power: {x**y}")
