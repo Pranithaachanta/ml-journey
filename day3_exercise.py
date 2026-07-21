4110e2bc3c64da7be1072bf44b7e5500ecd11ac7
Pranitha Achanta <pranithaachanta@gmail.com>
2026-07-21 01:11:41 -0600

    day-3 exercise

---
+++ day3_exercise.py
@@ -0,0 +1,7 @@
+story_name = "All I ever need was you"
+author = "someone"
+
+print(f"{story_name.upper()}")
+print(len(story_name))
+print(f"First word: {story_name[0:3]}")
+print(f"Reverse: {author[::-1]}")
